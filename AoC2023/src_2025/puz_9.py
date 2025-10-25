a47b05c529aab6cd6ef544f20bd9d044ddfd367a
Bo Jakobsen <boj@f77.dk>
2025-10-25 16:01:02 +0200

    Part 2 also, very similar just other formular

--- AoC2023/src_2025/puz_9.py
+++ AoC2023/src_2025/puz_9.py
@@ -1,12 +1,12 @@
 #with open('../testdata/9_testdata.dat') as f:
 with open('../data/9_data.dat') as f:
-    report = [list(map(int,list(x.split()))) for x in f]
-# load as list of lists
+    report = [list(map(int, list(x.split()))) for x in f]
+# load as list of lists of integers
 
 
 # Efficient diff of a list using zip operator
 def diff(dat):
-    res =  [y - x for x, y in zip(dat,dat[1:])]
+    res = [y - x for x, y in zip(dat, dat[1:])]
     return res
 
 
@@ -30,3 +30,26 @@ def part1():
 
 
 part1()
+
+
+# Part 1 and 2 could be done in one loop
+def part2():
+    res = 0
+    for hist in report:
+        # Diff until all differences are 0
+        all_zero = False
+        start_val = [hist[0]]  # store the last value of each step
+        while not all_zero:
+            hist = diff(hist)
+            start_val.append(hist[0])
+            if set(hist) == set([0]):  # test if hist is all 0
+                all_zero = True
+        # do the extrapolation back in time
+        next_val = 0
+        for val in start_val[::-1]:
+            next_val = val-next_val  # it is here it is different
+        res += next_val  # sum up result
+    print(res)
+
+
+part2()
