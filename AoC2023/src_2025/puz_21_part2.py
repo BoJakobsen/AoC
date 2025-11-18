09e8fb0160300a5f0512e390d6cf1d8a18ccb9b9
Bo Jakobsen <boj@f77.dk>
2025-11-18 17:29:41 +0100

    Working version, but clearly not a very generic way

--- AoC2023/src_2025/puz_21_part2.py
+++ AoC2023/src_2025/puz_21_part2.py
@@ -1,4 +1,5 @@
-from collections import deque 
+from collections import deque
+import numpy as np
 
 # Read maze type data
 #with open('../testdata/21_testdata.dat') as f:
@@ -57,27 +58,67 @@ Offset = Target % Nc # 65
 # in the Manhatten norm we get a nice diamond shape of the reachable points. 
 # 
 
-# first interesting configuration is N = Offset + 1 * Nc
-#      ^
-#     <*>
-#      v
-#  * is fully covered, <,>,^,V is "V" shaped end caps with 45deg cuts
+# There is the following type of blocks
 
-Nbase = count_reac(Offset + Nc)  # 33108
-
-# second interesting configuration is N = Offset + 2 * Nc
-# Telling how much is added
-
-#      ^
-#      #
-#    <#*#>
-#      #
-#      v
-
-N45 = count_reac(Offset + Nc * 2) - Nbase  # 58745
+# Full block
+# Corner block (always 4)
+# Edge type 1, 1/8 of a full block
+# Edge type 2, 7/8 of a full block
 
 
+# #### Manual counting on paper :-| and simulating 
+#
+# Offset + 1 * Nc
+# 1 x Full + 4 X Corner
+# + 4 X type 1 edge
+N1 = count_reac(Offset + Nc)  # 33108
+N1a = 0
 
+# Offset + 2 * Nc
+# 1 x Full + 4 X Corner
+# 4 x full  + 8 X type 1 edge + 4 X type 2 edge 
+N2 = count_reac(Offset + Nc * 2)  # 91853
+N2a = N2-N1
+
+# Offset + 3 *NC
+# 1 x Full + 4 X Corner
+# 12 x full  + 12 X type 1 edge + 8 X type 2 edge
 N3 = count_reac(Offset + Nc * 3)  # 179936
+N3a = N3-N1
 
-dN01 = N2 - Nbase
+# Offset + 4 *NC
+# 1 x Full + 4 X Corner
+# 24 x full  + 16 X type 1 edge + 12 X type 2 edge
+N4 = count_reac(Offset + Nc * 4)  # 297357
+N4a = N4-N1
+
+print(f'{N1a=}, {N2a=}, {N3a=}, {N4a=}')
+
+# Look at data,
+# x = Nc - 1, y is i the number of the type
+
+# For the full type: (got as a fit)
+# 2 * x^2  + 2*x  
+
+# for type 1 edge
+# 4 * (x)
+
+# for type 2 edge
+# 4 * (x)
+
+# With this definition  of x, edge are now handled as one, which is nice
+
+# Solve two eq with 2 unknowns 
+# Ntot - N1 = N_full * a + N_edge * b  # for two of the simulated states
+a = np.array([[4, 4], [12, 8 ]])
+b = np.array([N2a, N3a,])
+x = np.linalg.solve(a, b)
+
+# calculate the total number of full and edges
+N_full = 2 * (Nblocks-1) ** 2 + 2 * (Nblocks-1)
+N_edge = 4 * (Nblocks-1)
+
+# the total reachable plots are now found as:
+res = N_full * x[0] + N_edge * x[1] + N1
+print(int(res))
+
