a6b09404aaf3a1a1f2916e1b83b3e969a6f77dab
Bo Jakobsen <boj@f77.dk>
2025-12-07 12:50:05 +0100

    Slightly more commented version (some comments by claude.ai)

--- AoC2025/src/puz_06.py
+++ AoC2025/src/puz_06.py
@@ -1,19 +1,22 @@
 import math
 
 # single block of data
-#with open('../data/06_testdata.dat') as f:
+# with open('../data/06_testdata.dat') as f:
 with open('../data/06_data.dat') as f:
     lines = [x for x in f]
 
 
 def prob1():
+    """Process data horizontally: each row contains numbers followed by an operator."""
     dat = [l.strip().split() for l in lines]
-    problems = list(zip(*dat))
+    problems = list(zip(*dat))  # Transpose: columns become rows
     res = 0
+
     for problem in problems:
+        # Last element is the operator, rest are numbers
         if problem[-1] == '*':
             res += (math.prod(map(int, problem[:-1])))
-        else:
+        else:  # Assume '+'
             res += (sum(map(int, problem[:-1]))) 
 
     print('Prob 1; ', res)
@@ -23,23 +26,29 @@ prob1()
 
 
 def prob2():
+    """Process data vertically: read numbers column-wise, operators in last row."""
     res = 0
-    op = lines[-1].split()
-    kk = 0
-    numbs = []
+    op = lines[-1].split()  # Operators from last line
+    kk = 0  # Operator index
+    numbs = []  # Accumulate numbers for current operation
+
+    # Process each column position
     for c in range(len(lines[0])):
         string = ''
+        # Build vertical string from all rows except last (operators row)
         for l in range(len(lines) - 1):
             string += lines[l][c]
-        if any(char.isdigit() for char in string): # have number
+
+        if any(char.isdigit() for char in string):  # Column contains part of a number
             numbs.append(int(string))
-        else:
+        else:  # Hit a separator (spaces) - apply operation
             if op[kk] == '*':
                 res += math.prod(numbs)
             else:
                 res += sum(numbs)
-            kk += 1
-            numbs = []
+            kk += 1  # Move to next operator
+            numbs = []  # Reset for next group
+
     print('Prob 2: ', res)
 
 
