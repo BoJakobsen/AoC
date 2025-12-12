18d6613dbb81ad85e659383cb7119f3cd7c7bb4c
Bo Jakobsen <boj@f77.dk>
2025-12-12 08:38:21 +0100

    Retrospective plotting, shows why part 1 can be solved easily

--- AoC2025/src/puz_12.py
+++ AoC2025/src/puz_12.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 # Multiple groups, returned as a list of lists of strings
 with open('../data/12_data.dat') as f:
     groups = [group.splitlines() for group in f.read().split('\n\n')]
@@ -17,6 +19,7 @@ for lines in groups[-1]:
 
 # lets take a wild guess, that if the shapes take up less space than needed
 def part1():
+    DegreeFill = []
     res = 0
     for region in regions:
         A = region[0][0]*region[0][1]
@@ -26,10 +29,31 @@ def part1():
             for line in shapes[shape_id]:
                 Nfill_shape += line.count('#')
             Nfill += N_shape * Nfill_shape
+        DegreeFill.append((Nfill / A)*100)
         if Nfill <= A:
             res += 1
 
     print("Part 1: ", res)
+    return DegreeFill
+
+DegreeFill = part1()
 
 
-part1()
+# Plot histogram of DegreeFill with <100% highlighted in red
+plt.figure(figsize=(10, 6))
+# Separate data into <100 and >=100
+below_100 = [x for x in DegreeFill if x < 100]
+above_100 = [x for x in DegreeFill if x >= 100]
+
+# Create bins with 100% as a clear boundary
+bins = list(range(50, 100, 2)) + [100, 101]
+
+plt.hist(below_100, bins=bins, color='blue', alpha=0.7, edgecolor='black', label='< 100%')
+plt.hist(above_100, bins=bins, color='red', alpha=0.7, edgecolor='black', label='>= 100%')
+plt.axvline(100, color='black', linestyle='--', linewidth=2, label='100% threshold')
+plt.xlabel('Degree of Fill (%)')
+plt.ylabel('Frequency')
+plt.title('Histogram of DegreeFill')
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.show()
