c52d579033e7551d035834a358ab971639885cba
Bo Jakobsen <boj@boj.dk>
2026-07-30 10:51:21 +0200

    Better skeleton

--- helper_functions/skeleton.el
+++ helper_functions/skeleton.el
@@ -1,27 +1,41 @@
 ;;; puz_DD.el --- AOC YYYY Day DD -*- lexical-binding: t; -*-
 
-;; Uses functions from aoc-functions.el 
 
-;; If needed clean up from earlier work
-;;(puz-reset)
+;; add the AOC local helper function dir to the path
+(add-to-list 'load-path
+             (expand-file-name "../../helper_functions"
+                  (file-name-directory
+                   (or load-file-name buffer-file-name))))
+
+;; Reset all AOC (puz- name-space) and unload aoc-functions
+;(puz-reset)
+
+;; Requires the functions from aoc-functions.el
+(require 'aoc-functions) ; REMEMBER this does not reload changes from the file
 
 ; load into "*puz-scratch*" buffer
 (puz-load "../data/DD_data.dat")
 
-;;; For puzzle specific parses
-;; (defun puz-parse ()
-;;   "Parse `*puz-scratch*' "
-;;   (with-current-buffer "*puz-scratch*"
-;;     ))
-;; (puz-parse)  
+;; For problem specific parser
+(defun puz-parse ()
+  "Parse `*puz-scratch*' "
+  (with-current-buffer "*puz-scratch*"
+    (let ((parsed nil))
+      ( 
+         )
+      parsed)))
 
-;;; For grid problems
-;; (puz-grid-init)
+;;(puz-parse)  ; veal with C-x C-e to inspect parsed output
+
+;; For grid problem 
+; (puz-grid-init); sets puz-grid-n-cols, puz-grid-n-rows, puz-grid-offsets (8- nab)
 
 (defun puz-solve-part1 (parsed)
   "Solve Part 1"
   (with-current-buffer "*puz-scratch*"
-    
-    ))
+    (let ((res nil))
+      (
+       )
+      res)))
 
 (message "Solution for part 1 is = %d" (puz-solve-part1  (puz-parse)))
