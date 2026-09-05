30a15014637562422e23de3b3db86eeebd1bff11
Bo Jakobsen <boj@boj.dk>
2026-09-05 11:21:44 +0200

    Improved version conforming more to LISP standards

--- helper_functions/skeleton.el
+++ helper_functions/skeleton.el
@@ -1,41 +1,44 @@
 ;;; puz_DD.el --- AOC YYYY Day DD -*- lexical-binding: t; -*-
 
 
-;; add the AOC local helper function dir to the path
-(add-to-list 'load-path
-             (expand-file-name "../../helper_functions"
-                  (file-name-directory
-                   (or load-file-name buffer-file-name))))
+;;; Commentary:
+Advent of Code puzzel YYYY Day DD
+
+;;; Code:
 
 ;; Reset all AOC (puz- name-space) and unload aoc-functions
 ;;(puz-reset)
 
-;; Requires the functions from aoc-functions.el
+;; add the AOC local helper functions
+(add-to-list 'load-path
+             (expand-file-name "../../helper_functions"
+                               (file-name-directory
+                                (or load-file-name buffer-file-name))))
 (require 'aoc-functions) ; REMEMBER this does not reload changes from the file
 
-; load into "*puz-scratch*" buffer
+(require 'cl-lib)
+
+;; Load puzzle data into "*puz-scratch*" buffer
 (puz-load "../data/DD_data.dat")
+
+;; For grid problem 
+; (puz-grid-init); sets puz-grid-n-cols, puz-grid-n-rows, puz-grid-offsets (8- nab)
 
 ;; For problem specific parser
 (defun puz-parse ()
-  "Parse `*puz-scratch*'."
+  "Parse `*puz-scratch*' "
   (with-current-buffer "*puz-scratch*"
     (let ((parsed nil))
       ( 
          )
       parsed)))
 
-;;(puz-parse)  ; eval with C-x C-e to inspect parsed output
-
-;; For grid problem 
-; (puz-grid-init)  ; sets puz-grid-n-cols, puz-grid-n-rows, puz-grid-offsets (8- nab)
+;;(puz-parse)  ; veal with C-x C-e to inspect parsed output
 
 (defun puz-solve-part1 (parsed)
   "Solve Part 1"
-  (with-current-buffer "*puz-scratch*"
-    (let ((res nil))
-      (
-       )
-      res)))
+)
 
 (message "Solution for part 1 is = %S" (puz-solve-part1  (puz-parse)))
+
+;;; puz_DD.el ends here
