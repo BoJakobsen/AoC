3850f449834de787e28e94dc6b69ac2cb1904eb9
Bo Jakobsen <boj@boj.dk>
2026-09-20 12:58:38 +0200

    Cleaner version

--- AoC2025/src_lisp/puz_09.el
+++ AoC2025/src_lisp/puz_09.el
@@ -45,18 +45,18 @@
 
 
 ;; Setup utilities for compressing the grid into a 2d structure
+;; compression grid info as global vars
 (defvar puz-xs)
 (defvar puz-ys)
 (defvar puz-grid-width)
 (defvar puz-grid-height)
-;(defvar puz-grid)
 
-(let ((sorted (mapcar #'seq-uniq (mapcar #'sort  (apply #'cl-mapcar #'list (puz-parse))))))
-  (setq puz-xs (car sorted))
-  (setq puz-ys (cadr sorted))
-  (setq puz-grid-width (1+ (* 2 (length puz-xs))))
-  (setq puz-grid-height (1+ (* 2 (length puz-ys))))
-  )
+(defun puz-setup-compression ()
+    (let ((sorted (mapcar #'seq-uniq (mapcar #'sort  (apply #'cl-mapcar #'list (puz-parse))))))
+      (setq puz-xs (car sorted))
+      (setq puz-ys (cadr sorted))
+      (setq puz-grid-width (1+ (* 2 (length puz-xs))))
+      (setq puz-grid-height (1+ (* 2 (length puz-ys))))))
 
 (defun puz-compress-x (x)
   (1+ (* 2 (seq-position puz-xs x))))
@@ -64,37 +64,31 @@
 (defun puz-compress-y (y)
   (1+ (* 2 (seq-position puz-ys y))))
 
-;; (defun puz-compress (p)
-;;   (list (1+ (* 2 (seq-position puz-xs (car p)))) (1+ (* 2 (seq-position puz-ys (cadr p))))))
 (defun puz-compress (p)
   (list (puz-compress-x (car p)) (puz-compress-y (cadr p))))
 
 (defun puz-decode-x-y (p)
-  (+ (car p) (* (cadr p) puz-grid-width))
-  )
+  (+ (car p) (* (cadr p) puz-grid-width)))
 
 (defun puz-grid-print (grid)
     (cl-loop for n from 0  to (1- puz-grid-height)
-             do (message "%s \n " (seq-subseq grid (* n puz-grid-width) (* (1+ n) puz-grid-width)))
-             ))
+             do (message "%s \n " (seq-subseq grid (* n puz-grid-width) (* (1+ n) puz-grid-width)))))
 
 ;; Generate compressed grid
-(defun puz-generate-compressed-grid (parsed)
+(defun puz-generate-compressed-grid (pairs)
   (let* ((puz-grid (make-vector (* puz-grid-width  puz-grid-height) 0))
-         (pairs (puz-parse))
          (pairs-comp (mapcar #'puz-compress pairs))
-         (pairs-comp-wrap (append pairs-comp (list (car pairs-comp))))
-         )
+         (pairs-comp-wrap (append pairs-comp (list (car pairs-comp)))))
     (cl-loop for (p1 . rest) on pairs-comp-wrap
              when rest
              do (progn
                   (cl-loop for x from (min (car p1) (caar rest)) to (max (car p1) (caar rest)) 
-                           do (puz-grid-set (list x (cadr p1)) 1 ))
+                           do (aset puz-grid (puz-decode-x-y (list x (cadr p1)))  1 ))
                   (cl-loop for y from (min (cadr p1) (cadar rest)) to (max (cadr p1) (cadar rest))  
-                           do (puz-grid-set (list (car p1) y) 1 )))
+                           do (aset puz-grid (puz-decode-x-y (list (car p1) y)) 1 )))
              finally return puz-grid)))
 
-;; flood fill the outside, assuming no "hidden inner parts not part of the area"
+;; flood-fill the outside, assuming no "hidden inner parts not part of the area"
 (defun puz-grid-fill (grid)
  (let ((stack (list 0)))
   (aset grid 0 -1)
@@ -111,12 +105,11 @@
 
 
 (defun puz-grid-outside (grid)
-  "Mark the `outside' of the shape by 1's"
+  "Mark the `outside' of the shape by 1's for sat calculation."
     (cl-loop for p across-ref grid
              when (= p 1) do (setf p 0)
              when (= p -1) do (setf p 1)
-             finally return grid
-             ))
+             finally return grid))
 
 ;; Build: 2D prefix sum, summed-area table, might not the be most general implementation
 (defun puz-build-sat (grid)
@@ -132,33 +125,30 @@ Not totally general, we know that the shape is embedded in one empty block on al
     sat))
 
 (defun puz-get-overlap (sat x1 y1 x2 y2)
+  "Get overlap between X1,Y1,X2,Y2 and grid using SAT."
   (+ (aref sat (puz-decode-x-y (list (1+ x2) (1+ y2))))
      (* -1 (aref sat (puz-decode-x-y (list x1 (1+ y2)))))
      (* -1 (aref sat (puz-decode-x-y (list (1+ x2) y1))))
      (aref sat (puz-decode-x-y (list x1 y1)))))
 
-
 (defun puz-solve-part2 (parsed)
-  ""
+  "Solve part 2."
   (let* ((grid (puz-generate-compressed-grid parsed))
          (grid (puz-grid-fill grid))
          (grid (puz-grid-outside grid))
          (sat (puz-build-sat grid))
          )
-    ;(puz-grid-print sat)
-    ;(message "%s" (puz-get-overlap sat 2 2 6 6))
-                                        ;(puz-grid-print grid)
-    (cl-loop for ((x1 y1) . rest) on parsed ; combined "tail loop" and deconstruction
-             when rest ; needed as rest might be empty, which breaks inner loop
+    (cl-loop for ((x1 y1) . rest) on parsed  
+             when rest 
              maximize (cl-loop
                        for (x2 y2) in rest
-                       when (let ((x1c (puz-compress-x x1))
+                       when (let ((x1c (puz-compress-x x1)) 
                                   (x2c (puz-compress-x x2))
                                   (y1c (puz-compress-y y1))
                                   (y2c (puz-compress-y y2)))
-                              (= 0 (puz-get-overlap sat x1c y1c x2c y2c)))
+                              (= 0 (puz-get-overlap sat x1c y1c x2c y2c))) ; only count fully overlap
                        maximize (* (1+ (abs (- x1 x2)))(1+ (abs (- y1 y2))))))))
 
-(puz-solve-part2 (puz-parse))
+(message "Solution for part 2 is = %S" (progn (puz-setup-compression)   (puz-solve-part2  (puz-parse))))
 
 ;;; puz_09.el ends here
